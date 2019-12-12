ef8ba058f66c84307333596b03269496cf5beae0
Alan Rupp <alan.rupp@gmail.com>
2019-12-12 10:12:58 -0500

    updated manual doublet detection

--- python/scrublet.py
+++ python/scrublet.py
@@ -10,22 +10,35 @@ def find_expected_doublet_rate(mtx):
     lm = LinearRegression().fit(np.array(doublet_rate['cells']).reshape(-1,1),
                                 np.array(doublet_rate['rate']).reshape(-1,1))
     n_cells = mtx.shape[1]
-    expected_doublet_rate = lm.predict(n_cells)[0,0] / 100
+    expected_doublet_rate = lm.predict(np.array(n_cells).reshape(-1,1))[0,0] / 100
     return expected_doublet_rate
 
 def score_doublets(mtx, doublet_rate):
     scrub = scr.Scrublet(mtx.T, expected_doublet_rate=doublet_rate)
     doublet_scores, predicted_doublets = scrub.scrub_doublets()
-    doublets = scrub.call_doublets()
-    # if scrublet can't automatically call doublets, take top `doublet_rate` cells
-    def manual_doublets(scores):
+    def manual_threshold(scores):
         top_n = int(doublet_rate * len(scores))
         sorted_scores = np.sort(scores)
         threshold = sorted_scores[len(scores)-top_n:].min()
-        doublets = scores > threshold
-        return doublets
-    if doublets is None:
-        doublets = manual_doublets(doublet_scores)
-    elif doublet_rate * len(doublets) < sum(doublets):
-        doublets = manual_doublets(doublet_scores)
+        return threshold
+    # scrublet can be conservative -- making sure I get most doublets
+    if scrub.threshold_ > 0.3 and sum(predicted_doublets) < (doublet_rate * len(doublet_scores))/2:
+        threshold = manual_threshold(doublet_scores)
+        doublets = scrub.call_doublets(threshold=threshold)
+    else:
+        doublets = scrub.call_doublets()
     return doublets
+
+if __name__ == '__main__':
+    from scipy import io
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help='matrix folder location')
+    args = parser.parse_args()
+
+    # import mtx
+    mtx = io.mmread(f"{path}/matrix.mtx.gz")
+
+    # calcualte doublet rate and find doublets
+    doublet_rate = find_expected_doublet_rate(mtx)
+    doublets = score_doublets(mtx, doublet_rate)
